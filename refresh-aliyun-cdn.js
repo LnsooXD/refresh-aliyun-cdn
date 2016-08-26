8a826d35a3310d9bcf5facbecce6e46957f910d1
张少林 <LnsooXD@gmail.com>
2016-08-26 11:48:10 +0800

    change: fix some bugs

--- refresh-aliyun-cdn.js
+++ refresh-aliyun-cdn.js
@@ -1,3 +1,9 @@
+/*!
+ * refresh-aliyun-cdn - refresh-aliyun-cdn.js
+ * Copyright(c) 2016 LnsooXD <LnsooXD@gmail.com>
+ * MIT Licensed
+ */
+
 "use strict";
 if (typeof loadLib === 'undefined') {
 	require('iooly-cornerstone');
