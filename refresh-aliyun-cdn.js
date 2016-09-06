3745b09bb5694891c44a7f67e7f80c7aa8e6c0a1
张少林 <LnsooXD@gmail.com>
2016-09-06 21:51:48 +0800

    bugfix: remove private libs

--- refresh-aliyun-cdn.js
+++ refresh-aliyun-cdn.js
@@ -5,9 +5,6 @@
  */
 
 "use strict";
-if (typeof loadLib === 'undefined') {
-	require('iooly-cornerstone');
-}
 
-exports = module.exports = loadLib('./lib/cdn');
-exports.co = loadLib('./lib/co-cdn');
+exports = module.exports = require('./lib/cdn');
+exports.co = require('./lib/co-cdn');
